bf698c9af00029b59e747b8ecfed7e6d4a17ec17
Hongbin Yoon <popyto@naver.com>
2024-05-21 11:24:14 +0900

    Navbar.jsx 작성

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+
+export function Navbar() {
+  const navigate = useNavigate();
+  return (
+    <Flex gap={3}>
+      <Box
+        onClick={() => navigate("/")}
+        cursor={"pointer"}
+        _hover={{
+          bgColor: "gray.100",
+        }}
+      >
+        Home
+      </Box>
+      <Box
+        onClick={() => navigate("/write")}
+        cursor={"pointer"}
+        _hover={{
+          bgColor: "gray.200",
+        }}
+      >
+        글쓰기
+      </Box>
+    </Flex>
+  );
+}
