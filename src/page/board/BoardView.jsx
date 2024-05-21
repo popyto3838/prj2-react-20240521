28f17823838c17bd5bdb12872e9190830777a380
Hongbin Yoon <popyto@naver.com>
2024-05-21 15:11:43 +0900

    한게시물보기

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -1,13 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Box, FormControl, FormLabel, Input, Spinner } from "@chakra-ui/react";
 
 export function BoardView() {
   const { id } = useParams();
+  const [board, setBoard] = useState(null);
 
   useEffect(() => {
-    axios.get(`/api/board/${id}`);
+    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
 
-  return null;
+  if (board === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <Box>{board.id}번 게시물</Box>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input value={board.title} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Input value={board.content} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input value={board.writer} readOnly />
+        </FormControl>
+        <Box>
+          <FormControl>
+            <FormLabel>작성일시</FormLabel>
+            <Input type={"datetime-local"} value={board.inserted} readOnly />
+          </FormControl>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
