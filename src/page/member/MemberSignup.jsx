0492223c96ef116493a5458a95523490299b19a2
Hongbin Yoon <popyto@naver.com>
2024-05-22 10:24:31 +0900

    이메일 사용 가능한지 확인
    404-> 없으니까 사용가능
    200-> 있으니까 사용 불가능

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -4,6 +4,8 @@ import {
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
@@ -12,34 +14,36 @@ import { useNavigate } from "react-router-dom";
 
 export function MemberSignup() {
   const [email, setEmail] = useState("");
-  const [password, setPasword] = useState("");
+  const [password, setPassword] = useState("");
   const [nickName, setNickName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const toast = useToast();
   const navigate = useNavigate();
 
   function handleClick() {
+    setIsLoading(true);
     axios
       .post("/api/member/signup", { email, password, nickName })
       .then((res) => {
         toast({
           status: "success",
-          description: `회원 가입이 완료되었습니다`,
+          description: "회원 가입이 완료되었습니다.",
           position: "top",
         });
+        // todo : 로그인 화면으로 이동
         navigate("/");
       })
       .catch((err) => {
         if (err.response.status === 400) {
           toast({
             status: "error",
-            description: "입력값을 확인해주세요",
+            description: "입력값을 확인해 주세요.",
             position: "top",
           });
         } else {
           toast({
             status: "error",
-            description: `회원 가입 중 문제가 발생하였습니다`,
+            description: "회원 가입 중 문제가 발생하였습니다.",
             position: "top",
           });
         }
@@ -49,20 +53,50 @@ export function MemberSignup() {
       });
   }
 
+  function handleCheckEmail() {
+    axios
+      .get(`/api/member/check?email=${email}`)
+      .then((res) => {
+        toast({
+          status: "warning",
+          description: "사용할 수 없는 이메일입니다.",
+          position: "top",
+        });
+      }) //이미 있는 이메일
+      .catch((err) => {
+        if (err.response.status === 400) {
+          //사용할 수 있는 이메일
+          toast({
+            status: "info",
+            description: "사용할 수 있는 이메일 입니다.",
+            position: "top",
+          });
+        }
+      })
+      .finally();
+  }
+
   return (
     <Box>
-      <Box>회원가입</Box>
+      <Box>회원 가입</Box>
       <Box>
         <Box>
           <FormControl>
             <FormLabel>이메일</FormLabel>
-            <Input onChange={(e) => setEmail(e.target.value)} />
+            <InputGroup>
+              <Input onChange={(e) => setEmail(e.target.value)} />
+              <InputRightElement w={"75px"} mr={3}>
+                <Button onClick={handleCheckEmail} size={"sm"}>
+                  중복확인
+                </Button>
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
         </Box>
         <Box>
           <FormControl>
-            <FormLabel>비밀번호</FormLabel>
-            <Input onChange={(e) => setPasword(e.target.value)} />
+            <FormLabel>암호</FormLabel>
+            <Input onChange={(e) => setPassword(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
@@ -80,8 +114,6 @@ export function MemberSignup() {
             가입
           </Button>
         </Box>
-        <Box></Box>
-        <Box></Box>
       </Box>
     </Box>
   );
