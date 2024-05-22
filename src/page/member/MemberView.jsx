6af56c99e0f6bf2ea56cf7d4f8e6bd98a456817e
Hongbin Yoon <popyto@naver.com>
2024-05-22 15:18:13 +0900

    회원정보 보기

---
+++ src/page/member/MemberView.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+
+export function MemberView() {
+  return (
+    <Box>
+      <Box>회원정보</Box>
+      <Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Box>
+    </Box>
+  );
+}
